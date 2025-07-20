9b9a2564ba67c47d4842a5058503f7ee38c13823
aktob <92607805+aktob@users.noreply.github.com>
2025-07-20 19:49:59 +0300

    .

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps  // ✅ أضف السطر ده
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyAjgStbh-e-qcdbVrzM8c_FioP-8zGlXvA") // ✅ أضف السطر ده
+    GMSServices.provideAPIKey("api_key_value") // ✅ أضف السطر ده
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
